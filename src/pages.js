1522ea56a4274cd910f6dc7dec2fa96e3f9f2268
Eric Thomas <ericthomas@tuta.io>
2022-05-13 15:50:53 -0230

    placeholder for pages

---
+++ src/pages.js
@@ -0,0 +1,39 @@
+export function Home() {
+    return (
+        <div>
+            <h2>
+                Select a link to begin.
+            </h2>
+        </div>
+        );
+}
+
+export function Movies() {
+    return (
+        <div>
+            <h1>
+                This is the movies page
+            </h1>
+        </div>
+    );
+}
+
+export function AddMovie() {
+    return (
+        <div>
+            <h1>
+                This is the add movie page
+            </h1>
+        </div>
+    );
+}
+
+export function UhOh404() {
+    return (
+        <div>
+            <h2>
+                There's nothing here. Try one of the links.
+            </h2>
+        </div>
+    );
+}
