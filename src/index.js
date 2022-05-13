9f98ec79777341aa48b8a978e53975891b0abf7c
Eric Thomas <ericthomas@tuta.io>
2022-05-13 15:28:35 -0230

    refactor

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-const container = document.getElementById('root');
-const root = ReactDOM.createRoot(container);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <App />
