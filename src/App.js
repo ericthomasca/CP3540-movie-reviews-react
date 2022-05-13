1aa59ddee297f6875302d05b838c0e63658f9a72
Eric Thomas <ericthomas@tuta.io>
2022-05-13 15:28:07 -0230

    set up browserrouter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import './App.css';
+import {BrowserRouter, Link, Routes, Route} from 'react-router-dom';
+
 
 function App() {
   return (
-    <h1>Hello World!</h1>
+    <BrowserRouter>
+      <ul>
+        <li><Link to="/movies">Movies</Link></li>
+        <li><Link to="/add">Add a Movie</Link></li>
+      </ul>  
+      <Routes>
+        <Route path="/movies">
+          <Home/>
+        </Route> 
+        <Route path="/movies">
+          <Movies/>
+        </Route>  
+        <Route path="/add">
+          <AddMovie/>
+        </Route>
+        <Route path="*">
+          <UhOh404/>
+        </Route>   
+      </Routes>
+    </BrowserRouter>
   );
 }
 
