9a08d354d5b4ebbe623fb9e65596b2a5030146c7
Eric Thomas <ericthomas@tuta.io>
2022-05-16 20:57:36 -0230

    remove reduntant page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import { Routes, Route, Link } from "react-router-dom";
-import { Home, Reviews, AddReview, UhOh404 } from "./pages";
+import { Home, AddReview, UhOh404 } from "./pages";
 
 function App() {
   return (
@@ -8,12 +8,10 @@ function App() {
       <h1>Movie Reviews</h1>
         <nav>
           <Link to="">Home | </Link>
-          <Link to="reviews">Reviews | </Link>
           <Link to="add">Add Review</Link>
         </nav>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/reviews" element={<Reviews />} />
         <Route path="/add" element={<AddReview />} />
         <Route path="*" element={<UhOh404 />} />
       </Routes>
