cef7ec8e724df69e3cf6e65f44aa1685cace22a9
Eric Thomas <ericthomas@tuta.io>
2022-05-16 20:37:41 -0230

    reviews layout

--- src/pages.js
+++ src/pages.js
@@ -1,41 +1,50 @@
-export function Home() {
-    return (
-        <div>
-            <h2>
-                Select a link to begin.
-            </h2>
-        </div>
-        );
-}
+import reviewData from "./reviews.json";
 
-export function Reviews() {
-    const posterPrefix = "https://image.tmdb.org/t/p/w600_and_h900_bestv2";
-    return (
-        <div>
-            <h1>
-                This is the movies page { posterPrefix }
-            </h1>
-            <img src="img_girl.jpg" alt="Girl in a jacket"></img>
-        </div>
-    );
+export function Home() {
+  const posterPrefix = "https://image.tmdb.org/t/p/original";
+  return (
+    <div>
+      <table>
+        <tr>
+          <th>Movie</th>
+          <th>Release Date</th>
+          <th>Actors</th>
+          <th>Poster</th>
+          <th>Rating</th>
+        </tr>
+        {reviewData.map((review) => (
+          <tr key={review.id}>
+            <td>{review.name}</td>
+            <td>{review.release_date}</td>
+            <td>{review.actors.join(", ")}</td>
+            <td>
+              <img
+                src={posterPrefix + review.poster}
+                alt="poster"
+                height="150"
+                width="100"
+              ></img>
+            </td>
+            <td>{review.rating}</td>{" "}
+          </tr>
+        ))}
+      </table>
+    </div>
+  );
 }
 
 export function AddReview() {
-    return (
-        <div>
-            <h1>
-                This is the add movie page
-            </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h2>This is the add movie page</h2>
+    </div>
+  );
 }
 
 export function UhOh404() {
-    return (
-        <div>
-            <h2>
-                There's nothing here. Try one of the links.
-            </h2>
-        </div>
-    );
+  return (
+    <div>
+      <h2>There's nothing here. Try one of the links.</h2>
+    </div>
+  );
 }
